62bab034d2cdf1e7f950b8e4277802e7029ecb80
Golla Naveen Kumar <137159338+gollanaveenkumar2002@users.noreply.github.com>
2025-06-20 22:13:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/naveen-ai-verse.git/', // your repo name
+  base: '/portfolio/', // your repo name
   plugins: [react()],
   resolve: {
     alias: {
