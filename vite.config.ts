be7f3b99b591251f867d236536a7214080de5e13
Golla Naveen Kumar <137159338+gollanaveenkumar2002@users.noreply.github.com>
2025-08-08 16:58:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // <-- replace with your actual repo name
+  base: '/portfolio/', // <-- replace with your actual repo name
 })
